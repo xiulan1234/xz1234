785fec73ddf243c11f21b447db04ea321ddd89fd
xiulan1234 <50153775+xiulan1234@users.noreply.github.com>
2019-05-15 16:44:04 -0700

    Add files via upload

---
+++ nv_zip.py
@@ -0,0 +1,6 @@
+heights=[["Jenny", 61], ["Alexus", 70], ["Sam", 67], ["Grace", 64], ["Vik", 68]]
+ages=[["Aaron", 15], ["Dhruti", 16], ["Xiulan", 64], ["Kai", 28]]
+#zip to create new list, if ages only 2 lists, then only create 2 zip lists, not 4
+heights_and_ages =zip(heights, ages)
+#print (heights, ages)
+print (heights_and_ages)
