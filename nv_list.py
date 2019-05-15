67bf643f447944418d9fa8e36f5cca270b24eeaf
xiulan1234 <50153775+xiulan1234@users.noreply.github.com>
2019-05-15 15:34:48 -0700

    Add files via upload

---
+++ nv_list.py
@@ -0,0 +1,8 @@
+#this is commant
+job_applicants = ["jojn", "Geoff", "Brady", "Thomas"]
+Heights = [61, 70, 67, 64, 65]
+ints_and_strings=[1,2,3,'four', 'five']
+empty_list=[]
+
+print [job_applicants[1])
+print [job_applicants[2])
