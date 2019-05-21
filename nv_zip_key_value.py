af2a2c69eee9cda38aea06b7de978040e941c039
xiulan1234 <50153775+xiulan1234@users.noreply.github.com>
2019-05-20 23:24:47 -0700

    Add files via upload

---
+++ nv_zip_key_value.py
@@ -0,0 +1,4 @@
+tv_shows=['house', 'game of thrones', '24', "shark tank"]
+tv_shows_value =[89, 3, 40, 30]
+tv_shows_dict={key:value for key, value in zip(tv_shows, tv_shows_value)}
+print (tv_shows_dict)
