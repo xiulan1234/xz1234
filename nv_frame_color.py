5d3ff5cf7125e5dd37990665fe857639fc6f5bda
xiulan1234 <50153775+xiulan1234@users.noreply.github.com>
2019-05-21 18:55:49 -0700

    Add files via upload

---
+++ nv_frame_color.py
@@ -0,0 +1,9 @@
+import simplegui
+
+
+frame = simplegui.create_frame("Testing", 100, 100)
+#frame.add_button("Click me", click)
+frame.set_canvas_background("salmon")
+
+# Start the frame animation
+frame.start()
